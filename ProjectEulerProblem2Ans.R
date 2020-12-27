c8deca83d214efff28dbd4e7f54cb22e0bd4b558
David M Vermillion <social@davidmvermillion.com>
2020-12-27 13:43:53 -0500

    Shortened Attempt
    Tried to include even criteria inside while loop. Broke the script. Will return to try to fix at some point after new inspiration arrives.

--- ProjectEulerProblem2Ans.R
+++ ProjectEulerProblem2Ans.R
@@ -1,12 +1,16 @@
 # Utilizing hints on loops from:
 # https://eeecon.uibk.ac.at/~discdown/rprogramming/loops.html
 
+# First Working Iteration -------------------------------------------------
+
 # Initialize variables
+rm()
 N <- 4e6    # Max value
 x <- c(1, 2)   # Starting vector
 i <- 2      # Starting iteration counter
 
 # Calculate Fibonacci sequence to N total value
+StartTime <- Sys.time()
 while (x[i] < N) {
   if (x[i]+x[i-1] > N) break
   x <- c(x, x[i]+x[i-1])
@@ -20,6 +24,34 @@ x
 # https://www.quora.com/How-can-I-extract-even-odd-numbers-from-a-list-of-numbers-in-R
 e <- x%%2 == 0
 a <- x[e]
+EndTime <- Sys.time()
+EndTime - StartTime
 
 # Calculate and display answer
 sum(a)
+
+
+# More Compact Answer Attempt---------------------------------------------------
+
+# Line 48 breaks script
+
+# Initialize variables
+rm()
+N <- 4e6    # Max value
+x <- c(1, 2)   # Starting vector
+i <- 2      # Starting iteration counter
+
+# Calculate Fibonacci sequence to N total value
+StartTime <- Sys.time()
+while (x[i] < N) {
+  if (x[i]+x[i-1] > N) break
+  if (((x[i]+x[i-1]) %% 2) != 0) next
+  x <- c(x, x[i]+x[i-1])
+  i <- i+1
+}
+EndTime <- Sys.time()
+EndTime - StartTime
+
+# Display vector
+x
+
