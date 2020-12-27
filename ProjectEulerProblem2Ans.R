2b41b7adfd8e418ae8e0e190cc2ab839dd804cbe
David M Vermillion <social@davidmvermillion.com>
2020-12-27 13:01:37 -0500

    Completed code

--- ProjectEulerProblem2Ans.R
+++ ProjectEulerProblem2Ans.R
@@ -2,13 +2,24 @@
 # https://eeecon.uibk.ac.at/~discdown/rprogramming/loops.html
 
 # Initialize variables
-N <- 10 #4e6    # Max value
+N <- 4e6    # Max value
 x <- c(1, 2)   # Starting vector
 i <- 2      # Starting iteration counter
 
+# Calculate Fibonacci sequence to N total value
 while (x[i] < N) {
+  if (x[i]+x[i-1] > N) break
   x <- c(x, x[i]+x[i-1])
   i <- i+1
 }
 
+# Display vector
 x
+
+# Calculate and store even answers
+# https://www.quora.com/How-can-I-extract-even-odd-numbers-from-a-list-of-numbers-in-R
+e <- x%%2 == 0
+a <- x[e]
+
+# Calculate and display answer
+sum(a)
