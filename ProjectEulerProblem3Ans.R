c8ac8e4f2405aca2d8069a8204718f306ae3be32
David M Vermillion <social@davidmvermillion.com>
2021-01-03 18:02:30 -0500

    First Setup of test code
    Answer is slightly off. Code is progressing.

--- ProjectEulerProblem3Ans.R
+++ ProjectEulerProblem3Ans.R
@@ -7,14 +7,26 @@ x <- c(N,N-1)   # Starting vector
 i <- 2      # Starting iteration counter
 
 # Calculate all factors
-
 while (x[i] > 1) {
   #if (x[i]+x[i-1] > N) break
   x <- c(x, N/i)
   i <- i+1
 }
 
-x
+# Find Integer Answers
+# Inspiration from https://stackoverflow.com/questions/3476782/check-if-the-number-is-integer
+IntTest <- x%%1 == 0
+IntAns <- x[IntTest]
+IntAns    # Use for testing code at low values of N
+
+# Find Prime Answers
+PrimeTest <- IntAns%%2 != 0
+PrimeAns <- IntAns[PrimeTest]
+PrimeAns    # Use for testing code at low values of N
+
+# Find largest value of PrimeAns
+max(PrimeAns)
+
 
 EndTime <- Sys.time()
 EndTime - StartTime
