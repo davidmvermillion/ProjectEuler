8fd53acbf8cead20eeee64cef207fd2ccd54d6bb
David M Vermillion <social@davidmvermillion.com>
2021-01-10 14:56:37 -0500

    Complete Solution w/ Timing
    Runs in 0.5385592 seconds

--- ProjectEulerProblem4Ans.R
+++ ProjectEulerProblem4Ans.R
@@ -2,7 +2,8 @@
 
 rm(list=ls())
 
-
+StartTime <- Sys.time()
+N <- 3
 
 # Modifying Existing Solution ---------------------------------------------
 # From:
@@ -31,49 +32,6 @@ palindromeNum <- function(N){
   print(maxProd)
 }
 
-
-
-
-# Test Cases --------------------------------------------------------------
-
-# https://stackoverflow.com/questions/26508519/how-to-add-elements-to-a-list-in-r-loop
-n1 <- 100000    # Lower bound
-N1 <- 999999    # Upper bound
-rangeEval <- N1-n1
-n2 <- 100
-N2 <- 999
-i <- 1    # Initialization counter value
-j <- 1
-l <- vector("list", rangeEval)    # Initialization list
-# Create list of test cases
-for (i in n1:N1) {
-  if (l[[i]]%%2)
-  for (j in n:N){
-    l[[j]] <- i*j
-    j <- j + 1
-  }
-  # l[[i]] <- i^2
-  # i <- i + 1
-}
-
-# https://stackoverflow.com/questions/33004238/r-removing-null-elements-from-a-list
-l1 <- l[-which(sapply(l, is.null))]    # Remove nulls from list
-
-
-# Palidromes in Range -----------------------------------------------------
-i <- 1
-ans <- vector("list", length(l1))
-for (i in l1) {
-  char <- as.character(l1[i])
-  char1 <- substr(char,1,1)
-  char2 <- substr(char,2,2)
-  char5 <- substr(char,4,4)
-  char6 <- substr(char,5,5)
-  if (char1 == char6 & char2 == char5) {
-    ans[i] <- sqrt(i)
-  }
-  i <- i + 1
-}
-ans1 <- ans[-which(sapply(ans, is.null))]
-
-
+palindromeNum(3)
+EndTime <- Sys.time()
+EndTime - StartTime
