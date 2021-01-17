b905c267bf228a91f0bc084df50257be29026be4
David M Vermillion <social@davidmvermillion.com>
2021-01-17 14:27:03 -0500

    That got basically nowhere
    No output

--- ProjectEulerProblem5Ans.R
+++ ProjectEulerProblem5Ans.R
@@ -4,29 +4,142 @@ rm(list=ls())
 
 StartTime <- Sys.time()
 
-TestLim <- 10000
+TestLim <- 1000000
 N <- 1
 Targ <- 20
 poss <- c(1,2)
 
 for (i in 1:TestLim){
-  # if (i %% 2 == 0) {
-  #   
-  # }
-  for (a in 2:Targ) { # Ignoring 1 since all integers divide by 1
-    if (i %% a == 0) {
-      N <- i
-      a <- a + 1
-      i <- i + 1
-      next
+  if (i %% 2 == 0) {
+    if (i %% 3 == 0) {
+      if (i %% 4 == 0){
+        if (i %% 5 == 0) {
+          if (i %% 6 == 0){
+            if (i %% 7 == 0){
+              if (i %% 8 == 0){
+                if (i %% 9 == 0){
+                  if (i %% 10 == 0){
+                    if (i %% 11 == 0){
+                      if (i %% 12 == 0){
+                        if (i %% 13 == 0){
+                          if (i %% 14 == 0){
+                            if (i %% 15 == 0){
+                              if (i %% 16 == 0){
+                                if (i %% 17 == 0){
+                                  if (i %% 18 == 0){
+                                    if (i %% 19 == 0){
+                                      if (i %% 20 == 0){
+                                        print(i)
+                                      }
+                                      else{
+                                        i <- i + 1
+                                        break
+                                      }
+                                    }
+                                    else{
+                                      i <- i + 1
+                                      break
+                                    }
+                                  }
+                                  else{
+                                    i <- i + 1
+                                    break
+                                  }
+                                }
+                                else{
+                                  i <- i + 1
+                                  break
+                                }
+                              }
+                              else{
+                                i <- i + 1
+                                break
+                              }
+                            }
+                            else{
+                              i <- i + 1
+                              break
+                            }
+                          }
+                          else{
+                            i <- i + 1
+                            break
+                          }
+                        }
+                        else{
+                          i <- i + 1
+                          break
+                        }
+                      }
+                      else{
+                        i <- i + 1
+                        break
+                      }
+                    }
+                    else{
+                      i <- i + 1
+                      break
+                    }
+                  }
+                  else{
+                    i <- i + 1
+                    break
+                  }
+                }
+                else{
+                  i <- i + 1
+                  break
+                }
+              }
+              else{
+                i <- i + 1
+                break
+              }
+            }
+            else{
+              i <- i + 1
+              break
+            }
+          }
+          else{
+            i <- i + 1
+            break
+          }
+        }
+        else{
+          i <- i + 1
+          break
+        }
+      }
+      else{
+        i <- i + 1
+        break
+      }
     }
     else{
-      a <- a + 1
       i <- i + 1
       break
     }
   }
-  poss <- c(poss, N)
+  else{
+    i <- i + 1
+    break
+  }
+  }
+  # for (a in 2:Targ) { # Ignoring 1 since all integers divide by 1
+  #   if (i %% a == 0) {
+  #     N <- i
+  #     a <- a + 1
+  #     i <- i + 1
+  #     next
+  #   }
+  #   else{
+  #     a <- a + 1
+  #     i <- i + 1
+  #     break
+  #   }
+  # }
+  # poss <- c(poss, N)
 }
 
 EndTime <- Sys.time()
