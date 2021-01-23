9995dbfa98dc08af3c7987e25c10a248ce67088a
David M Vermillion <social@davidmvermillion.com>
2021-01-23 13:51:42 -0500

    Trying something else
    Creating a list of whole numbers evenly divisible by specific numbers. While loop seems a bit over the top. Moving to skip counting.

--- ProjectEulerProblem5Ans.R
+++ ProjectEulerProblem5Ans.R
@@ -7,6 +7,17 @@ StartTime <- Sys.time()
 TestLim <- 10000000
 i <- 1
 
+TestLim1 <- 10000
+x <- c(1, 2)
+i <- 2
+
+while (x[i] < TestLim1) {
+  if (x[i]%%2!=0) break
+  x <- c(x, i)
+  i <- i+1
+}
+
+
 while (i < TestLim){
   if (i%%2==0 & i%%3==0 & i%%4==0 & i%%5==0 & i%%6==0 & i%%7==0 &
       i%%8==0 & i%%9==0 & i%%10==0 & i%%11==0 & i%%12==0 & i%%13==0 &
