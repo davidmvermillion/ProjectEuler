5a77730902715c98e8802572c1485ca5c76df52b
David M Vermillion <social@davidmvermillion.com>
2021-01-24 18:13:09 -0500

    Not efficient
    This runs through in 28.27217s. That's not efficient at all. Max possible result is factorial(20). That's still way bigger than the expected final answer, though. Perhaps reverse the order and go backwards?

--- ProjectEulerProblem5Ans.R
+++ ProjectEulerProblem5Ans.R
@@ -4,7 +4,7 @@ rm(list=ls())
 
 StartTime <- Sys.time()
 
-TestLim <- 10000
+TestLim <- 100000000
 
 # Generate sequence of numbers divisible by specific numbers
 List2 <- seq(2,TestLim,2)
@@ -48,52 +48,6 @@ T1.15 <- intersect(T1.14, List17)
 T1.16 <- intersect(T1.15, List18)
 T1.17 <- intersect(T1.16, List19)
 T1.18 <- intersect(T1.17, List20)
-
-
-
-# Manually intersecting lists, round 2
-
-T2.1 <- intersect(T1.1, T1.2)
-T2.2 <- intersect(T1.3, T1.4)
-T2.3 <- intersect(T1.5, T1.6)
-T2.4 <- intersect(T1.7, T1.8)
-T2.5 <- intersect(T1.9, List20)
-
-# Convert to dataframe
-
-d2 <- as.data.frame(List2)
-d3 <- as.data.frame(List3)
-d4 <- as.data.frame(List4)
-d5 <- as.data.frame(List5)
-d6 <- as.data.frame(List6)
-d7 <- as.data.frame(List7)
-d8 <- as.data.frame(List8)
-d9 <- as.data.frame(List9)
-d10 <- as.data.frame(List10)
-d11 <- as.data.frame(List11)
-d12 <- as.data.frame(List12)
-d13 <- as.data.frame(List13)
-d14 <- as.data.frame(List14)
-d15 <- as.data.frame(List15)
-d16 <- as.data.frame(List16)
-d17 <- as.data.frame(List17)
-d18 <- as.data.frame(List18)
-d19 <- as.data.frame(List19)
-d20 <- as.data.frame(List20)
-
-
-
-# Next up:
-# Make data frames and perform an inner join or
-
-# Combine lists into array
-# Call array for interative intersections
-# Lowest resultant intersection should be answer
-
-for (j in 2:20){
-  l1 <- 
-  intersect(List)
-}
 
 
 EndTime <- Sys.time()
