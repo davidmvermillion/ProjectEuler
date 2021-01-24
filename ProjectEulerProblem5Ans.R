bf9f7b38136cd636cafa6f94218811ed028c5954
David M Vermillion <social@davidmvermillion.com>
2021-01-23 21:51:02 -0500

    Next Idea
    Making a list of numbers and then finding intersection to answer question

--- ProjectEulerProblem5Ans.R
+++ ProjectEulerProblem5Ans.R
@@ -5,7 +5,6 @@ rm(list=ls())
 StartTime <- Sys.time()
 
 TestLim <- 10000
-i <- 1
 
 # Generate sequence of numbers divisible by specific numbers
 List2 <- seq(2,TestLim,2)
@@ -15,29 +14,28 @@ List5 <- seq(5,TestLim,5)
 List6 <- seq(6,TestLim,6)
 List7 <- seq(7,TestLim,7)
 List8 <- seq(8,TestLim,8)
+List9 <- seq(9,TestLim,9)
+List10 <- seq(10,TestLim,10)
+List11 <- seq(11,TestLim,11)
+List12 <- seq(12,TestLim,12)
+List13 <- seq(13,TestLim,13)
+List14 <- seq(14,TestLim,14)
+List15 <- seq(15,TestLim,15)
+List16 <- seq(16,TestLim,16)
+List17 <- seq(17,TestLim,17)
+List18 <- seq(18,TestLim,18)
+List19 <- seq(19,TestLim,19)
+List20 <- seq(20,TestLim,20)
 
 
-# TestLim1 <- 1000
-# x <- c(1, 2)
-# i <- 2
-# 
-# while (x[i] < TestLim1) {
-#   x <- c(x, i)
-#   i <- i+2
-# }
+# Next up:
+# Combine lists into array
+# Call array for interative intersections
+# Lowest resultant intersection should be answer
 
-
-while (i < TestLim){
-  if (i%%2==0 & i%%3==0 & i%%4==0 & i%%5==0 & i%%6==0 & i%%7==0 &
-      i%%8==0 & i%%9==0 & i%%10==0 & i%%11==0 & i%%12==0 & i%%13==0 &
-      i%%14==0 & i%%15==0 & i%%16==0 & i%%17==0 & i%%18==0 & i%%19==0 & i%%20==0
-      ){
-    print(i)
-    break
-  }
-  else{
-    i <- i + 1
-  }
+for (j in 2:20){
+  l1 <- 
+  intersect(List)
 }
 
 
